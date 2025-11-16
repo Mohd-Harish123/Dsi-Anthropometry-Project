bf16ad67b34f19c0a664049072e3a19238c6e616
Aman Kesharwani <amankesharwani9919@gmail.com>
2025-11-17 01:42:34 +0530

    cleaned the code

--- application/app/_layout.tsx
+++ application/app/_layout.tsx
@@ -3,68 +3,7 @@ import { Stack } from "expo-router";
 
 export default function TabsLayout() {
   return (
-//     <Tabs
-//       screenOptions={{
-//         headerShown: false,
-//       }}
-//     >
-//       <Tabs.Screen
-//         name="login"
-//         options={{ 
-//           title: "Login",
-//           href: null,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="dashboard"
-//         options={{ 
-//           title: "Dashboard",
-//           href: null,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="anthroscan"
-//         options={{ 
-//           title: "AnthroScan",
-//           href: null,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="profile"
-//         options={{ 
-//           title: "Profile",
-//           href: null,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="doctor_profile"
-//         options={{ 
-//           title: "Doctor Profile",
-//           href: null,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="doctor_dashboard"
-//         options={{ 
-//           title: "Doctor Dashboard",
-//           href: null,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="results"
-//         options={{ 
-//           title: "Results",
-//           href: null,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="measurement_report"
-//         options={{ 
-//           title: "Measurement Report",
-//           href: null,
-//         }}
-//       />
-//     </Tabs>
+
 <Stack>
       <Stack.Screen name="LoginRegister" options={{ title: 'Home' }} />
       <Stack.Screen name="about" options={{ title: 'About' }} />
